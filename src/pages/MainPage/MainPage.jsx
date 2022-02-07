b49b16b30413e960a410adabc2e7b4f4762ffcc6
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-02-07 12:19:11 -0800

    Reorganize fetch calls and limit results to 50 with splice

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import Board from "../../components/Board/Board"
 
 function MainPage() {
-  const [searchTerm, setSearchTerm] = useState('sunflower');
-  const [allItems, setAllItems] = useState([])
+  const [searchTerm, setSearchTerm] = useState('cats');
+  const [allItems, setAllItems] = useState([]);
 
 
   // fetch objectIDs from API
@@ -11,10 +11,12 @@ function MainPage() {
     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchTerm}`)
     .then(res => res.json())
     .then(allItems => {
-      setAllItems(allItems.objectIDs)
+      setAllItems(allItems.objectIDs.splice(0, 50))
       console.log(allItems.objectIDs);
     })
   }, [])
+
+
 
   return (
     <main>
