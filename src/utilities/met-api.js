08bf27a2fe9947ba7a4e2d051290d7e6d7c7e171
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-02-07 19:59:06 -0800

    Update metAPI file syntax - file not yet in use

--- src/utilities/met-api.js
+++ src/utilities/met-api.js
@@ -1,9 +1,10 @@
 const BASE_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
 const SEARCH = '/search?';
 
-  async function fetchAllItems() {
+  async function fetchAllItems(searchTerm) {
     const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/search?q=${searchTerm}`)
-    .then(res => res.json().objectIDs.splice(0, 50));
+    const shortItemList = allItems.objectIDs.splice(0, 50)
+    return shortItemList
     // .then(allItems => {
     //   const shortItemList = allItems.objectIDs.splice(0, 50)
     // })
@@ -11,7 +12,7 @@ const SEARCH = '/search?';
   }
 
 async function fetchItemByID(itemID) {
-  const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+  const res = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${itemID}`)
   // this will return the data than I can then set to a variable in my front end function
   return res.json();
 }
