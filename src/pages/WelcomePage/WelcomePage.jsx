eb0600ae4aa20c84874903f980775e4a037e19c5
Megan Ratto <meganratto@Megans-MacBook-Air.local>
2022-02-12 22:34:27 -0800

    Begin welcome page styling

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
+import "./WelcomePage.css"
 import AuthPage from "../AuthPage/AuthPage";
 import SlideShow from "../../components/SlideShow/SlideShow"
 
 function WelcomePage({ setUser}) {
   return (
     <div className="WelcomePage">
-      <div className="container">
-        <h1>Welcome to ArtLab</h1>
-        <h2>Where you can view and save your favorite pieces from the MET's massive collection</h2>
-        <AuthPage setUser={setUser} />
-        {/* <Link to="/signup">Sign Up</Link> */}
-      </div>
-      <div>
-        <SlideShow />
+      <div className="welcome-container">
+
+        <div className="welcome-text">
+          <h1 className="welcome-title">Welcome to ArtLab</h1>
+          <h2 className="welcome-text">Where you can view and save your favorite pieces from the MET's massive collection</h2>
+          <AuthPage setUser={setUser} />
+          {/* <Link to="/signup">Sign Up</Link> */}
+        </div>
+
+        <div className="welcome-slideshow">
+          <SlideShow />
+        </div>
       </div>
     </div>
   )
